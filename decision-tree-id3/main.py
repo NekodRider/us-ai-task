fc0a02269aa67d8c19b02b19238ee1b396aa784f
NekodRider <driver2223@gmail.com>
2017-11-08 01:29:02 +0800

    dT-id3 nearly done

--- decision-tree-id3/main.py
+++ decision-tree-id3/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import csv
 import math
 
@@ -19,8 +20,8 @@ def proc_data(raw_train_data,raw_test_data):
     data_mean=(raw_test_data.mean()['Age']*raw_test_data.size+raw_train_data.mean()['Age']*raw_train_data.size)/(raw_test_data.size+raw_train_data.size)
     train_data = train_data.fillna({'Age':data_mean })
     test_data = test_data.fillna({'Age': data_mean})
-
-
+    train_data['Age'] = train_data['Age'].apply(lambda x:int(x/10))
+    test_data['Age'] = test_data['Age'].apply(lambda x:int(x/10))
     train_data['Sex'] = train_data['Sex'].map(sexDict)
     test_data['Sex'] = test_data['Sex'].map(sexDict)
     # data['Embarked'] = data['Embarked'].map(embarkedDict)
@@ -55,21 +56,59 @@ def get_split_feature_set(train_data):
     return feature_dict
 
 
-def get_best(fname,data_set):
+def get_split_feature_entropy(fname,data_set):
     data_num = data_set.shape[0]
     feature_entropy = {}
-    feature_prob = {}
+    color={0:'r',1:'g'}
     for feature,feature_num in dict(data_set.groupby(fname).size()).items():
         label_set = dict(data_set[data_set[fname]==feature].groupby("Survived").size())
         entropy_tmp = 0
+        width = 0.3
+        i = -0.15
         for key, val in label_set.items():
             prob = val / feature_num
             entropy_tmp -= feature_num/data_num * prob * math.log(prob, 2)
+            plt.bar(feature+i, val, width=width,label=key,color=color[key])
+            i=i+0.3
+
         feature_entropy[feature]=entropy_tmp
+    plt.xticks(list(dict(data_set.groupby(fname).size()).keys()))
+    plt.title(fname)
+    # plt.show()
     result = 0
-    for val in feature_entropy:
+    for val in feature_entropy.values():
         result+=val
     return result
+
+
+def generate_tree(former_tree_node,train_data):
+    if train_data.shape[1]==1:
+        former_tree_node['end']=train_data.iloc[0,0]
+        return
+    data_set = get_split_feature_set(train_data)
+    entropy_total = get_entropy(trainData)
+    # print(entropy_total)
+    entropy_dict = {}
+    for key in data_set:
+        data = (data_set[key])
+        entropy_dict[key] = entropy_total - (get_split_feature_entropy(key, data))
+    best = max(entropy_dict, key=entropy_dict.get)
+    former_tree_node[best]={}
+    feat_list=list(train_data.columns)
+    feat_list.remove(best)
+    for i in dict(train_data.groupby(best).size()).keys():
+        former_tree_node[best][i]={}
+        generate_tree(former_tree_node[best][i],train_data[feat_list][train_data[best] == i])
+
+
+def get_end(data_row,tree):
+    key = list(tree.keys())[0]
+    res = tree[key][data_row[key]]
+    if 'end' not in list(res.keys()):
+        return get_end(data_row,res)
+    else:
+        return res['end']
+
 
 
 rawTrainData = pd.DataFrame(pd.read_csv('train.csv'))
@@ -77,10 +116,15 @@ rawTestData = pd.DataFrame(pd.read_csv('test.csv'))
 idList = np.array(rawTestData.ix[:, 'PassengerId'])
 trainData, testData = proc_data(rawTrainData, rawTestData)
 # print(trainData)
-setToCalc = get_split_feature_set(trainData)
-entropy_total = get_entropy(trainData)
-print(entropy_total)
-entropy_dict = {}
-for key in setToCalc:
-    entropy_dict[key]=(get_best(key,setToCalc[key]))
-print(entropy_dict)
+
+decisionTree={}
+generate_tree(decisionTree,trainData)
+print(decisionTree)
+count = 0
+countF = 0
+for _,row in trainData.iterrows():
+    result = get_end(row,decisionTree)
+    if result!=row['Survived']:
+        countF=countF+1
+    count = count + 1
+print(countF/count)
